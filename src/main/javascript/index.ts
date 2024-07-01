5bff004cc611bf00b0897a87cc71c71759d8dd20
Phan Huy Hoang <hoangph271@gmail.com>
2024-07-01 09:40:51 +0700

    stat measurements.txt

--- src/main/javascript/index.ts
+++ src/main/javascript/index.ts
@@ -1,17 +1,22 @@
-const FILE_PATH = 'data/measurements.txt'
-
-const fileHandler = await Deno.open(FILE_PATH)
-
-const fileReader = fileHandler.readable
-
-let bytesCount = 0
-
 const start = Date.now()
 
-for await (const chunk of fileReader) {
-    bytesCount += chunk.byteLength
-}
+const FILE_PATH = 'data/measurements.txt'
 
-console.info(bytesCount)
+// const fileHandler = await Deno.open(FILE_PATH)
+
+// const fileReader = fileHandler.readable
+
+// let bytesCount = 0
+
+
+// for await (const chunk of fileReader) {
+//     bytesCount += chunk.byteLength
+// }
+
+// console.info(bytesCount)
+
+const fileStats = await Deno.stat(FILE_PATH)
+
+console.info(fileStats.size)
 
 console.info(Date.now() - start)
--- src/main/rust/src/main.rs
+++ src/main/rust/src/main.rs
@@ -1,3 +1,33 @@
+use directories::UserDirs;
+use std::{fs, os::unix::fs::MetadataExt, time::Instant};
+
+const FILE_PATH: &str = "useCode/1brc/data/measurements.txt";
+
 fn main() {
-    println!("Hello, world!");
+    let start_at = Instant::now();
+
+    let binding = UserDirs::new().unwrap();
+    let home_dir = binding.home_dir();
+
+    let measurements_path = home_dir.join(FILE_PATH);
+
+    let medadata = fs::metadata(&measurements_path).expect(&format!("Can't open FILE {}", measurements_path.to_string_lossy()));
+    println!("{}", medadata.size());
+
+    // let file = File::open(measurements_path).unwrap();
+
+    // let reader = BufReader::new(file);
+    // let mut lines_count = 0;
+
+    // for line in reader.lines() {
+    //     if let Ok(_line) = line {
+    //         lines_count += 1;
+    //     } else {
+    //         panic!("Error reading lines");
+    //     }
+    // }
+
+    // println!("Total lines count: {}", lines_count);
+
+    println!("Total run time: {:?}", start_at.elapsed());
 }
